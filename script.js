ceae9f257e5e3a291f6408ab355b2c35914b52fd
Moritz Meyer <moritzmaximilian@gmx.net>
2022-10-08 13:58:26 +0200

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-if(localStorage.getItem('cookieSeen') != 'shown') {
-    $('.cookieDiv').delay(2000).fadeIn();
-}
-
-$('.close').click(function() {
-    $('.cookieDiv').fadeOut();
-    localStorage.setItem('cookieSeen', 'shown');
-})
-
